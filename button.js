c721ed02896eddf3d4a13a2f8d25918016fd8146
dngwoo <akfmzk84@naver.com>
2020-07-01 12:22:14 +0900

    MainMenu update

--- button.js
+++ button.js
@@ -1,5 +1,6 @@
 const buttons = document.querySelectorAll("button");
-const container = document.querySelectorAll(".sidebar__submenus");
+const containers = document.querySelectorAll(".sidebar__submenus");
+const subMenus = document.querySelectorAll(".sidebar__link");
 
 // console.log(buttons, buttons[1]);
 // console.log(typeof buttons);
@@ -10,11 +11,20 @@ buttons.forEach(function (button, index) {
     button.classList.toggle("open");
     if (button.className.includes("open")) {
       button.nextElementSibling.style.transform = "rotate(180deg)";
-      container[index].style.display = "block";
+      containers[index].style.display = "block";
     } else {
       button.nextElementSibling.style.transform = "rotate(0deg)";
-      container[index].style.display = "none";
+      containers[index].style.display = "none";
     }
   };
   button.addEventListener("click", changeButtonClassName);
+});
+
+subMenus.forEach((subMenu) => {
+  const changeSubMenuColor = () => {
+    if (subMenu.href === location.href) {
+      subMenu.style.color = "#04456a";
+    }
+  };
+  subMenu.addEventListener("click", changeSubMenuColor);
 });
